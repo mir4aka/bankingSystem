6b0f50d738775bad22d41996b0e7db329ab8ebf2
mir4aka <mirkoo858@gmail.com>
2023-01-11 14:40:50 +0200

    source folder

---
+++ src/main/java/eu/deltasource/BankInstitution.java
@@ -0,0 +1,14 @@
+package eu.deltasource;
+
+import java.util.ArrayList;
+
+public class BankInstitution {
+    private String bankName;
+    private String bankAddress;
+
+    public BankInstitution(String bankName, String bankAddress) {
+        this.bankName = bankName;
+        this.bankAddress = bankAddress;
+    }
+    
+}
