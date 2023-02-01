0b82eb8c2ac50a07d79699e1bb51d64f7321b964
mir4aka <mirkoo858@gmail.com>
2023-02-01 23:37:56 +0200

    added some javadoc

--- src/main/java/eu/deltasource/BankService.java
+++ src/main/java/eu/deltasource/BankService.java
@@ -7,6 +7,9 @@ import eu.deltasource.model.Transactions;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * This is the bank service class which is called if a transaction is going to be fulfilled(deposit, withdraw,transfer).
+ */
 public class BankService {
 
     public void depositMoneyToAccount(BankAccount account, double amountToDeposit, LocalDate date) {
@@ -28,16 +31,32 @@ public class BankService {
         account.addTransaction(transaction);
     }
 
+    /**
+     * The method below takes care of the transaction between two bank accounts
+     *
+     * @param sourceAccount
+     * @param targetAccount
+     * @param amountToTransfer
+     * @param date
+     */
     public void transferMoney(BankAccount sourceAccount, BankAccount targetAccount, double amountToTransfer, LocalDate date) {
         transfer(sourceAccount, targetAccount, amountToTransfer, date);
     }
 
+    /**
+     * This is the logic for the transfer method.
+     *
+     * @param sourceAccount
+     * @param targetAccount
+     * @param amountToTransfer
+     * @param date
+     */
     private void transfer(BankAccount sourceAccount, BankAccount targetAccount, double amountToTransfer, LocalDate date) {
         checkValidAccounts(sourceAccount, targetAccount, amountToTransfer);
 
         double fees = calculateFees(sourceAccount, targetAccount);
 
-        double exchangeRate = calculateExchangeRate(sourceAccount, targetAccount, amountToTransfer);
+        double exchangeRate = calculateExchangeRate(sourceAccount, targetAccount);
 
         String sourceAccountCurrency = sourceAccount.getCurrency();
         String targetAccountCurrency = targetAccount.getCurrency();
@@ -83,6 +102,13 @@ public class BankService {
         return moneyAvailable;
     }
 
+    /**
+     * This method calculates the fee of the amount to be transferred whether it's to a different bank from the source bank or not and returns the value.
+     *
+     * @param sourceAccount
+     * @param targetAccount
+     * @return
+     */
     private double calculateFees(BankAccount sourceAccount, BankAccount targetAccount) {
         double fees = 0;
 
@@ -94,7 +120,14 @@ public class BankService {
         return fees;
     }
 
-    private double calculateExchangeRate(BankAccount sourceAccount, BankAccount targetAccount, double amountToDeposit) {
+    /**
+     * This method calculates the exchange rate between banks and returns the value.
+     *
+     * @param sourceAccount
+     * @param targetAccount
+     * @return
+     */
+    private double calculateExchangeRate(BankAccount sourceAccount, BankAccount targetAccount) {
         double exchangeRate;
 
         if (!sourceAccount.getCurrency().equals(targetAccount.getCurrency())) {
@@ -115,6 +148,17 @@ public class BankService {
         }
     }
 
+    /**
+     * This method creates a new transaction which is being updated on both accounts - source and target.
+     * This way I can keep up with the transactions and store them in a collection.
+     *
+     * @param sourceAccount
+     * @param targetAccount
+     * @param date
+     * @param amountToBeDepositedToTargetAccount
+     * @param exchangeRate
+     * @param amountToBeWithdrawnFromSourceAccount
+     */
     private void updatingSourceAccountAndTargetAccountTransactions(BankAccount sourceAccount, BankAccount targetAccount, LocalDate date, double amountToBeDepositedToTargetAccount, double exchangeRate, double amountToBeWithdrawnFromSourceAccount) {
         List<Transactions> sourceAccountTransactions = sourceAccount.getAccountTransactions();
         List<Transactions> targetAccountTransactions = targetAccount.getAccountTransactions();
@@ -136,6 +180,14 @@ public class BankService {
         }
     }
 
+    /**
+     * This method takes care of the deposit transaction and adds the transaction to the same collection as the method above.
+     *
+     * @param account
+     * @param amount
+     * @param date
+     * @return
+     */
     private Transactions updateDepositTransaction(BankAccount account, double amount, LocalDate date) {
         Transactions transaction = new Transactions();
 
@@ -148,6 +200,14 @@ public class BankService {
         return transaction;
     }
 
+    /**
+     * This method takes care of the withdraw transaction and adds the transaction to the same collection as the method above.
+     *
+     * @param account
+     * @param amount
+     * @param date
+     * @return
+     */
     private Transactions updateWithdrawTransaction(BankAccount account, double amount, LocalDate date) {
         Transactions transaction = new Transactions();
 
@@ -160,6 +220,16 @@ public class BankService {
         return transaction;
     }
 
+    /**
+     * This method updates source's account transactions (exchange rate, source bank, target bank, etc...)
+     *
+     * @param sourceAccount
+     * @param targetAccount
+     * @param amount
+     * @param exchangeRate
+     * @param date
+     * @return
+     */
     private Transactions updateSourceAccountTransactions(BankAccount sourceAccount, BankAccount targetAccount, double amount, double exchangeRate, LocalDate date) {
         Transactions transaction = new Transactions();
         transaction.setExchangeRate(exchangeRate);
@@ -176,6 +246,16 @@ public class BankService {
         return transaction;
     }
 
+    /**
+     * This method updates target's account transactions (exchange rate, source bank, target bank, etc...)
+     *
+     * @param sourceAccount
+     * @param targetAccount
+     * @param amount
+     * @param exchangeRate
+     * @param date
+     * @return
+     */
     private Transactions updateTargetAccountTransactions(BankAccount sourceAccount, BankAccount targetAccount, double amount, double exchangeRate, LocalDate date) {
         Transactions transaction = new Transactions();
         transaction.setExchangeRate(exchangeRate);
@@ -192,6 +272,13 @@ public class BankService {
         return transaction;
     }
 
+    /**
+     * This method checks if the source account's balance is insufficient for a transfer, if the customer is trying to transfer money to his own bank account,
+     * if the customer is trying to transfer money between accounts different from `Current Account`.
+     * @param sourceAccount
+     * @param targetAccount
+     * @param amountToDeposit
+     */
     private void checkValidAccounts(BankAccount sourceAccount, BankAccount targetAccount, double amountToDeposit) {
         if (!sourceAccount.getAccountTypes().contains(AccountTypes.CURRENT_ACCOUNT) || !targetAccount.getAccountTypes().contains(AccountTypes.CURRENT_ACCOUNT)) {
             throw new TransfersAllowedBetweenCurrentAccountsException("Transfers are only allowed between two Current accounts.\n");
@@ -206,24 +293,38 @@ public class BankService {
         }
     }
 
+    /**
+     * This method checks if the source account and target account's currencies are the same or not. Therefore, the amountToDeposit is being calculated
+     * based on the result.
+     * @param amountToDeposit
+     * @param sourceAccountCurrency
+     * @param targetAccountCurrency
+     * @return
+     */
     private double checkCurrenciesAndCalculateTheAmountToBeDeposited(double amountToDeposit, String sourceAccountCurrency, String targetAccountCurrency) {
         if (sourceAccountCurrency.equals("BGN")) {
             if (targetAccountCurrency.equals("USD")) {
                 amountToDeposit = amountToDeposit * 0.55;
             } else if (targetAccountCurrency.equals("GBP")) {
                 amountToDeposit = amountToDeposit * 0.45;
+            } else if (targetAccountCurrency.equals("BGN")){
+                amountToDeposit = amountToDeposit * 1;
             }
         } else if (sourceAccountCurrency.equals("USD")) {
             if (targetAccountCurrency.equals("BGN")) {
                 amountToDeposit = amountToDeposit * 1.80;
             } else if (targetAccountCurrency.equals("GBP")) {
                 amountToDeposit = amountToDeposit * 0.80;
+            } else if (targetAccountCurrency.equals("USD")) {
+                amountToDeposit = amountToDeposit * 1;
             }
         } else if (sourceAccountCurrency.equals("GBP")) {
             if (targetAccountCurrency.equals("BGN")) {
                 amountToDeposit = amountToDeposit * 2.23;
             } else if (targetAccountCurrency.equals("USD")) {
                 amountToDeposit = amountToDeposit * 1.24;
+            } else if (targetAccountCurrency.equals("GBP")) {
+                amountToDeposit = amountToDeposit * 1;
             }
         }
         return amountToDeposit;
